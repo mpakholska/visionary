40f88ea7086293d29533387fa26fca34d9895338
mpakholska <marta.pakholska.work@gmail.com>
2024-12-13 12:19:59 +0200

    init

---
+++ constants/images.js
@@ -0,0 +1,7 @@
+import profile from "../assets/images/profile.png";
+import thumbnail from "../assets/images/thumbnail.png";
+import cards from "../assets/images/cards.png";
+import path from "../assets/images/path.png";
+import empty from "../assets/images/empty.png";
+
+export default { profile, thumbnail, cards, path, empty };
